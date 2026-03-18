e3a5c6012b8512d1ddb9c4db648654a71854435a
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-18 11:27:55 +0500

    feat: typescrit lessons started

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fullName = 'Asilbek';
+console.log(typeof fullName);
+const salary = 10000;
+const bonus = 5000;
+const total = salary + bonus;
+console.log(total);
+let isMarried = true;
---
+++ app.ts
@@ -0,0 +1,9 @@
+let fullName: string = 'Asilbek'
+console.log(typeof fullName)
+
+const salary: number = 10000
+const bonus: number = 5000
+const total: number = salary + bonus
+console.log(total)
+
+let isMarried: boolean = true;
