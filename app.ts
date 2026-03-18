c924e94f21d209ad692ee9246ebbc52f13b2d0ad
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-18 21:02:53 +0500

    feat: objects type

--- app.ts
+++ app.ts
@@ -1,3 +1,19 @@
+const person = {
+    firstName: "Asilbek", // required
+    lastName: "Karomatov", //required
+    city: "Bukhara",
+    skills: {
+        programming: "typescript",
+        design: "Figma",
+    }
+}
+
+function getName(data: { firstName: string; lastName: string }): string {
+    return `${data.firstName} ${data.lastName}`;
+}
+
+const person1 = getName(person)
+console.log(person1)
 /*
 function getName(firstName: string, lastName: string): string {
     return `${firstName} ${lastName}`;
