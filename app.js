a4b34645edb40513f7c46077b3636e6e433b3e30
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-18 11:35:37 +0500

    ignore: remove js files

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let fullName = 'Asilbek';
-console.log(typeof fullName);
-const salary = 10000;
-const bonus = 5000;
-const total = salary + bonus;
-console.log(total);
-let isMarried = true;
