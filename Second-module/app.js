d9e7c63d166bef1af678290fefbaa91b501b2ff9
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-04-05 17:24:17 +0500

    type guards & asserts

--- Second-module/app.js
+++ Second-module/app.js
@@ -1,21 +1,48 @@
 "use strict";
-// null - hech qanday qiymat yo'q, qolda ozimiz beramiz
-//undefined - ozgaruvchiga qiymat berilmagan
+//Type Casting (Type Assertion) -> Tur bilan ishlash
 Object.defineProperty(exports, "__esModule", { value: true });
-let username = null;
-//Nullish coalescing operator
-let showUsername = username ?? 'Guest';
-console.log(showUsername);
-function logger(msg) {
-    if (msg !== null) {
-        console.log(msg.toUpperCase());
+let message = "Hello World!";
+let strLength1 = message.length; //Angle bracket syntax
+console.log(strLength1);
+let strLength2 = message; // as syntax
+console.log(strLength2.length);
+function getInfo(vehicle) {
+    if ('speed' in vehicle) {
+        console.log(`Speed: ${vehicle.speed}km/h`);
     }
     else {
-        console.log('No message provider');
+        console.log(`Altitude: ${vehicle.altitude}metres`);
     }
 }
-logger('Hello World!');
-logger(null);
-let x = null;
-console.log(x);
+getInfo({ speed: 100 });
+getInfo({ altitude: 300 });
+class Dog {
+    bark() {
+        console.log('Woof!');
+    }
+}
+class Cat {
+    meow() {
+        console.log('Meow!');
+    }
+}
+function makeSound(animal) {
+    if (animal instanceof Dog) {
+        animal.bark();
+    }
+    else {
+        animal.meow();
+    }
+}
+makeSound(new Dog());
+makeSound(new Cat());
+//Asserts -> Tasdiqlash
+function logNumber(value) {
+    if (typeof value !== "number") {
+        throw new Error('value is not a number');
+    }
+}
+const age = 20;
+logNumber(age);
+console.log(age + 10);
 //# sourceMappingURL=app.js.map
--- Second-module/app.ts
+++ Second-module/app.ts
@@ -1,21 +1,65 @@
-// null - hech qanday qiymat yo'q, qolda ozimiz beramiz
-//undefined - ozgaruvchiga qiymat berilmagan
+//Type Casting (Type Assertion) -> Tur bilan ishlash
 
-let username: string | null = null;
+let message: unknown = "Hello World!"
 
-//Nullish coalescing operator
-let showUsername = username ?? 'Guest'
-console.log(showUsername)
+let strLength1: number = (<string>message).length //Angle bracket syntax
+console.log(strLength1)
 
-function logger(msg: string | null) {
-    if(msg !== null){
-        console.log(msg.toUpperCase());
-    } else{
-    console.log('No message provider');}
+let strLength2: string = message as string // as syntax
+console.log(strLength2.length)
+
+// Type guards -> Turi bilan tekshirish
+
+type Car = {
+    speed: number;
 }
 
-logger('Hello World!');
-logger(null)
+type Plane = {
+    altitude: number;
+}
 
-let x: null = null
-console.log(x)
+function getInfo(vehicle: Car | Plane){
+    if('speed' in vehicle){
+        console.log(`Speed: ${vehicle.speed}km/h`);
+    }else{
+        console.log(`Altitude: ${vehicle.altitude}metres`);
+    }
+}
+
+getInfo({speed: 100})
+getInfo({altitude: 300})
+
+class Dog{
+    bark(){
+        console.log('Woof!')
+    }
+}
+
+class Cat{
+    meow(){
+        console.log('Meow!')
+    }
+}
+
+function makeSound(animal: Dog | Cat) {
+    if(animal instanceof Dog){
+        animal.bark()
+    }else{
+        animal.meow()
+    }
+}
+
+makeSound(new Dog())
+makeSound(new Cat())
+
+//Asserts -> Tasdiqlash
+
+function logNumber(value: unknown): asserts value is number {
+    if(typeof value !== "number"){
+        throw new Error('value is not a number')
+    }
+}
+
+const age: unknown = 20
+logNumber(age)
+console.log(age + 10)
