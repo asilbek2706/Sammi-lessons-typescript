b1f714a72630c3686f4b3d009371434d65ff776f
Asilbek <asilbekkaromatov2@gmail.com>
2026-05-31 11:49:13 +0500

    Project

--- third-module/app.js
+++ third-module/app.js
@@ -1,82 +1,92 @@
 "use strict";
-/*
-class Person{
-    name: string;
-
-    constructor(name: string){
-        this.name = name;
-    }
-
-    greeting(){
-        this.name = 'Hello World';
-    }
-}*/
 Object.defineProperty(exports, "__esModule", { value: true });
-/*class Counter{
-    count: number = 1;
-
-    increment(){
-        console.log(this)
-        console.log(this.count);
-        this.count++;
+// Role enum
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role["STUDENT"] = "STUDENT";
+})(Role || (Role = {}));
+//User class
+class User {
+    id;
+    name;
+    role;
+    constructor(id, name, role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
     }
 }
-
-const counter = new Counter();
-const incrementFn = counter.increment.bind(counter);
-
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()*/
-/*class Counter {
-    count: number = 1
-
-    increment = () => {
-        console.log(this)
-        console.log(this.count)
-        this.count++
+//Course class
+class Course {
+    students = [];
+    id;
+    title;
+    description;
+    constructor(id, title, description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+    // Getter - students list
+    get studentList() {
+        return this.students.map((student) => student.name).join(', ');
+    }
+    // Add student to course
+    enrollStudent(student) {
+        if (student.role === Role.STUDENT) {
+            this.students.push(student);
+            console.log(`Student ${student.name} enrolled in course ${this.title}`);
+        }
+        else {
+            console.log('Only students can be enrolled in course');
+        }
     }
 }
-
-const counter = new Counter()
-const incrementFn = counter.increment
-
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()*/
-/*
-
-class User{
-    name: string = 'Asilbek'
-
-    log(this: User){
-        console.log(this.name);
+class Admin extends User {
+    constructor(id, name) {
+        super(id, name, Role.ADMIN);
     }
-}*/
-class Calculator {
-    value = 0;
-    add(num) {
-        this.value += num;
-        return this;
+    // Add course
+    addCourse(course, courses) {
+        courses.push(course);
+        console.log(`Course ${course.title} added!`);
     }
-    subtract(num) {
-        this.value -= num;
-        return this;
+    // Student List of course
+    getStudentList(course) {
+        console.log(`Students in course ${course.title}: ${course.studentList}`);
     }
-    multiply(num) {
-        this.value *= num;
-        return this;
-    }
-    getValue() {
-        return this.value;
+    // get single course
+    getCourse(courseId, courses) {
+        const course = courses.find((course) => course.id === courseId);
+        return course;
     }
 }
-const calc = new Calculator();
-const result = calc.add(5).subtract(3).multiply(4).getValue();
-console.log(result);
-console.log(calc.getValue());
+class Student extends User {
+    constructor(id, name) {
+        super(id, name, Role.STUDENT);
+    }
+    enroll(course) {
+        course.enrollStudent(this);
+    }
+}
+// Backtest
+const courses = [];
+const admin = new Admin(101, 'Admin');
+const student1 = new Student(201, 'Ali');
+const student2 = new Student(202, 'Osman');
+const course1 = new Course(101, 'Math', 'Math course');
+admin.addCourse(course1, courses);
+student1.enroll(course1);
+student2.enroll(course1);
+admin.getStudentList(course1);
+const findingCourse = admin.getCourse(101, courses);
+if (findingCourse) {
+    console.log(`Course found: ${findingCourse.title} - ${findingCourse.description}`);
+}
+else {
+    console.log('Course not found!');
+}
+const noCourse = admin.getCourse(999, courses);
+console.log(`Course not found: ${noCourse}`);
 //# sourceMappingURL=app.js.map
--- third-module/app.ts
+++ third-module/app.ts
@@ -1,89 +1,108 @@
-/*
-class Person{
-    name: string;
+// Role enum
+enum Role {
+    ADMIN = 'ADMIN',
+    STUDENT = 'STUDENT',
+}
 
-    constructor(name: string){
-        this.name = name;
-    }
+//User class
+class User {
+    id: number
+    name: string
+    role: Role
 
-    greeting(){
-        this.name = 'Hello World';
-    }
-}*/
-
-/*class Counter{
-    count: number = 1;
-
-    increment(){
-        console.log(this)
-        console.log(this.count);
-        this.count++;
+    constructor(id: number, name: string, role: Role) {
+        this.id = id
+        this.name = name
+        this.role = role
     }
 }
 
-const counter = new Counter();
-const incrementFn = counter.increment.bind(counter);
+//Course class
+class Course {
+    private students: User[] = []
 
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()*/
+    id: number
+    title: string
+    description: string
 
-/*class Counter {
-    count: number = 1
+    constructor(id: number, title: string, description: string) {
+        this.id = id
+        this.title = title
+        this.description = description
+    }
 
-    increment = () => {
-        console.log(this)
-        console.log(this.count)
-        this.count++
+    // Getter - students list
+    get studentList(): string {
+        return this.students.map((student) => student.name).join(', ')
+    }
+
+    // Add student to course
+    enrollStudent(student: User): void {
+        if (student.role === Role.STUDENT) {
+            this.students.push(student)
+            console.log(`Student ${student.name} enrolled in course ${this.title}`)
+        } else {
+            console.log('Only students can be enrolled in course')
+        }
     }
 }
 
-const counter = new Counter()
-const incrementFn = counter.increment
-
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()
-incrementFn()*/
-/*
-
-class User{
-    name: string = 'Asilbek'
-
-    log(this: User){
-        console.log(this.name);
-    }
-}*/
-
-class Calculator {
-    value: number = 0
-
-    add(num: number): this {
-        this.value += num
-        return this
+class Admin extends User {
+    constructor(id: number, name: string) {
+        super(id, name, Role.ADMIN)
     }
 
-    subtract(num: number) :this{
-        this.value -= num
-        return this
+    // Add course
+    addCourse(course: Course, courses: Course[]): void {
+        courses.push(course)
+        console.log(`Course ${course.title} added!`)
     }
 
-    multiply(num:number): this{
-        this.value *= num
-        return this
+    // Student List of course
+    getStudentList(course: Course): void {
+        console.log(`Students in course ${course.title}: ${course.studentList}`)
     }
 
-    getValue():number{
-        return this.value
-
+    // get single course
+    getCourse(courseId: number, courses: Course[]): Course {
+        const course = courses.find((course) => course.id === courseId)
+        return course as Course
     }
 }
 
+class Student extends User {
+    constructor(id: number, name: string) {
+        super(id, name, Role.STUDENT)
+    }
 
-const calc = new Calculator()
-const result = calc.add(5).subtract(3).multiply(4).getValue()
-console.log(result)
-console.log(calc.getValue())
+    enroll(course: Course): void {
+        course.enrollStudent(this)
+    }
+}
+
+// Backtest
+const courses: Course[] = []
+const admin = new Admin(101, 'Admin')
+
+const student1 = new Student(201, 'Ali')
+const student2 = new Student(202, 'Osman')
+
+const course1 = new Course(101, 'Math', 'Math course')
+
+admin.addCourse(course1, courses)
+student1.enroll(course1)
+student2.enroll(course1)
+
+admin.getStudentList(course1)
+
+const findingCourse = admin.getCourse(101, courses)
+
+if (findingCourse) {
+    console.log(`Course found: ${findingCourse.title} - ${findingCourse.description}`)
+} else {
+    console.log('Course not found!')
+}
+
+
+const noCourse = admin.getCourse(999, courses)
+console.log(`Course not found: ${noCourse}`)
