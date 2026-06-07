478ee508b1efe59b80d288d5244774a31aab5aa7
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-07 18:55:43 +0500

    #40. Readonly, Partial, Required

--- fourth-module/app.js
+++ fourth-module/app.js
@@ -1,60 +1,24 @@
 "use strict";
-// Form Validation
 Object.defineProperty(exports, "__esModule", { value: true });
-// #3. Form Validation Function
-function validateForm(form) {
-    const errors = {};
-    // Email validation
-    if (!form.email) {
-        errors.email = 'Email is required';
-    }
-    else if (form.email.includes('@') === false) {
-        errors.email = 'Email is invalid';
-    }
-    // Password validation
-    if (!form.password) {
-        errors.password = 'Password is required';
-    }
-    else if (form.password.length < 6) {
-        errors.password = 'Password must be at least 6 characters';
-    }
-    // Name validation
-    if (!form.name) {
-        errors.name = 'Name is required';
-    }
-    else if (form.name.length < 3) {
-        errors.name = 'Name must be at least 3 characters';
-    }
-    return errors;
+const product = {
+    name: 'Shoes',
+    price: 200,
+    id: 1,
+    excerpt: 'This is a nice pair of shoes',
+}; // We cannot change our product
+console.log(product);
+/*
+function createProduct(product: Required<IProduct>) {
+    console.log(product)
 }
-// #4. Testing
-const formData = {
-    email: 'test@gmail.com',
-    password: '456',
-    name: 'Te',
-};
-const erors = validateForm(formData);
-console.log(erors);
-function isEmailValid(form) {
-    return form.email.includes('@');
+
+createProduct(product);
+
+function updateProduct(updatedProduct: Partial<IProduct>) {
+    const updateProduct = { ...product, ...updatedProduct }
+    console.log(updateProduct)
 }
-const isValidEmail = isEmailValid(formData);
-console.log(isValidEmail);
-function validateDynamicForm(form) {
-    const errors = {};
-    Object.keys(form).forEach((key) => {
-        if (!form[key]) {
-            errors[key] = `${key} is required`;
-        }
-    });
-    return errors;
-}
-const dynamicFormData = {
-    email: 'test@gmail.com',
-    password: '',
-    name: '',
-    customId: '',
-};
-const dynamicErrors = validateDynamicForm(dynamicFormData);
-console.log(dynamicErrors);
+
+updateProduct({price: 300});
+*/
 //# sourceMappingURL=app.js.map
--- fourth-module/app.ts
+++ fourth-module/app.ts
@@ -1,90 +1,34 @@
-// Form Validation
-
-// #1. Data type
-type FormDataType = {
-    name: string
-    email: string
-    password: string
+//Partial
+interface IProduct{
+    id: number;
+    name: string;
+    price: number;
+    excerpt?: string;
 }
 
-// #2. Error type
-type FormErrorType = {
-    email?: string
-    password?: string
-    name?: string
+type ReadonlyProduct = Readonly<IProduct>;
+
+const product: ReadonlyProduct = {
+    name: 'Shoes',
+    price: 200,
+    id: 1,
+    excerpt: 'This is a nice pair of shoes',
+} // We cannot change our product
+
+console.log(product);
+
+
+/*
+function createProduct(product: Required<IProduct>) {
+    console.log(product)
 }
 
-// #3. Form Validation Function
-function validateForm<T extends FormDataType>(form: T): FormErrorType {
-    const errors: FormErrorType = {}
+createProduct(product);
 
-    // Email validation
-    if (!form.email) {
-        errors.email = 'Email is required'
-    } else if (form.email.includes('@') === false) {
-        errors.email = 'Email is invalid'
-    }
-
-    // Password validation
-    if (!form.password) {
-        errors.password = 'Password is required'
-    } else if (form.password.length < 6) {
-        errors.password = 'Password must be at least 6 characters'
-    }
-
-    // Name validation
-    if (!form.name) {
-        errors.name = 'Name is required'
-    } else if (form.name.length < 3) {
-        errors.name = 'Name must be at least 3 characters'
-    }
-
-    return errors
+function updateProduct(updatedProduct: Partial<IProduct>) {
+    const updateProduct = { ...product, ...updatedProduct }
+    console.log(updateProduct)
 }
 
-// #4. Testing
-const formData: FormDataType = {
-    email: 'test@gmail.com',
-    password: '456',
-    name: 'Te',
-}
-
-const erors = validateForm(formData)
-console.log(erors)
-
-// #5. Conditional type
-type IsEmailValid<T> = T extends { email: string } ? boolean : never
-
-function isEmailValid<T extends FormDataType>(form: T): IsEmailValid<T> {
-    return form.email.includes('@') as IsEmailValid<T>
-}
-
-const isValidEmail = isEmailValid(formData)
-console.log(isValidEmail)
-
-// #6. Dynamic fields
-type DynamicFormData = {
-    [key: string]: string
-}
-
-function validateDynamicForm<T extends DynamicFormData>(form: T): FormErrorType {
-    const errors: FormErrorType = {}
-
-    Object.keys(form).forEach((key) => {
-        if (!form[key]) {
-            errors[key] = `${key} is required`
-        }
-    })
-
-    return errors
-}
-
-const dynamicFormData = {
-    email: 'test@gmail.com',
-    password: '',
-    name: '',
-    customId: '',
-}
-
-const dynamicErrors = validateDynamicForm(dynamicFormData)
-console.log(dynamicErrors)
+updateProduct({price: 300});
+*/
