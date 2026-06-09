38a54cc8d5bef0b70d5376b4ff4442e2b01b6120
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-09 21:46:08 +0500

    Joriy Dars #42. ReturnType, Parametr, Awaited

--- fourth-module/app.ts
+++ fourth-module/app.ts
@@ -2,41 +2,42 @@
 // Omit <T, K> - Asosiy tipdan kerakli propertilarni olib tawlaydi
 //Extract <T, U> - ikkita tip orasida umumiy propertilarni tanlab olish uchun
 // Exclude <T, U> - bitta tipdan ikkinchi tipdagi propertilarni olib tawlaydi
+// Readonly <T> - Asosiy tipni oqiladigan qilib olish uchun
+// Partial <T> - Asosiy tipni toliq yoki qisman toldirish uchun
+// Required <T> - Asosiy tipni toliq yoki qisman toldirish uchun
 
-// interface IAnimal{
-//     name: string;
-//     species: string;
-//     age: number;
-// }
-//
-// interface IPlant{
-//     name: string;
-//     color: string;
-//     age: number;
-// }
-//
-// // type CommonProperties = Extract<keyof IAnimal, keyof IPlant>
-// type CommonProperties = Exclude<keyof IPlant, keyof IAnimal>
+//ReturnType <T> - Funksiyadan qaytgan qiymatning tipini olish uchun
+// Parameters <T> - Funksiyadan parameter tipini olish uchun
+// ConstructorParameters <T> - Clasdsan constructor parameterlarining tipini olish
+// Awaited <T> - Promise qaytaradigan funksiyadan qaytgan qiymatning tipini olish
 
-/*interface IUser {
-    name: string
-    age: number
-    address: string
-    email: string
-    avatar: string
-    phone: string
-    password: string
-    secretKey: string
+/*
+function calc(a: number, b: number): number {
+    return a + b
 }
 
-// type UserContactInfo = Pick<IUser, 'email' | 'address' | 'name' | 'age'>
-type UserContactInfo = Omit<IUser, 'password' | 'secretKey'>
+type calcParams = Parameters<typeof calc>
+type CalcType = ReturnType<typeof calc>
 
-const contact: UserContactInfo = {
-    address: 'wjhdn',
-    age: 22,
-    name: 'skncms ',
-    email: 'skncms.com',
-    phone: '38574853i',
-    avatar: 'https://avatars2.githubusercontent.com/u/64?v=4',
-}*/
+const params: calcParams = [1, 2]
+// const result: CalcType = calc(5, 8)
+const result: CalcType = calc(...params)
+console.log(result)
+*/
+
+/*
+class Person{
+    constructor(public name: string, public age: number){}
+}
+
+type PersonParams = ConstructorParameters<typeof Person>
+
+const personParams: PersonParams = ['John', 30]
+const person = new Person(...personParams)
+console.log(person)*/
+
+async function fetchDaata(): Promise<string> {
+    return 'Data'
+}
+
+type FetchDataReturnType = Awaited<ReturnType<typeof fetchDaata>>
